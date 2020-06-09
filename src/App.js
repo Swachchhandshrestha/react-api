ea602f9610d343d3cd98d36dab2fafdaeba2503b
swachchhandshrestha <swachchhandshrestha11@gmail.com>
2020-06-09 14:51:17 +0545

    rendering data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-class App extends Component{
+class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -7,18 +7,38 @@ class App extends Component{
             isLoaded: false,
         }
     }
+
     componentDidMount() {
-        fetch('')
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    item: json,
+                })
+            });
     }
 
-    render() {
-    return (
-        <div className="App">
-
-        </div>
-
-    );
-  }
+    render()
+    {
+        var {isLoaded, items} = this.state;
+        if (!isLoaded) {
+            return <div>Loading...</div>;
+        }
+        else {
+            return (
+                <div className="App">
+                    <ul>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                Name:{item.name} | Email:{item.email}
+                            </li>
+                        ))};
+                    </ul>
+                </div>
+            );
+        }
+    }
 }
 
 export default App;
