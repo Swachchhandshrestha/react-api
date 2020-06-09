98c3694fc3e97b76eddbdf99683dfef47ee7a2c9
swachchhandshrestha <swachchhandshrestha11@gmail.com>
2020-06-09 18:36:21 +0545

    Updates

---
+++ precache-manifest.f497af0881cef0276bd651a78ba58071.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64e5d9e4da45909aae251634b070c51",
+    "url": "/react-api/index.html"
+  },
+  {
+    "revision": "dde1df6bccdfd5693434",
+    "url": "/react-api/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "208485d265c8e911db1a",
+    "url": "/react-api/static/js/2.32baebbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-api/static/js/2.32baebbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dde1df6bccdfd5693434",
+    "url": "/react-api/static/js/main.fdae3293.chunk.js"
+  },
+  {
+    "revision": "944913692bd851a668c5",
+    "url": "/react-api/static/js/runtime-main.9cccb668.js"
+  }
+]);
